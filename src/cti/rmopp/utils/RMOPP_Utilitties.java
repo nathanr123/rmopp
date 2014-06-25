6bb9edc1cf7af33317e65aa0f737b520c3742a16
nathanr_kamal <nathanr_kamal@KAMALANATHAN>
2014-06-25 15:12:07 +0530

    latest

--- src/cti/rmopp/utils/RMOPP_Utilitties.java
+++ src/cti/rmopp/utils/RMOPP_Utilitties.java
@@ -1,6 +1,7 @@
 package cti.rmopp.utils;
 
 import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.Toolkit;
 
 import cti.rmopp.model.RMOPP_User;
@@ -10,8 +11,14 @@ public class RMOPP_Utilitties {
 	private static double scrWidth;
 
 	private static double scrHeight;
-	
+
 	private static RMOPP_User currentUser;
+
+	private static boolean isInputPortSelected = false;
+
+	private static Point startXY, endXY;
+
+	private static boolean drawFlag = false;
 
 	public enum Nagigation {
 
@@ -65,12 +72,71 @@ public class RMOPP_Utilitties {
 	}
 
 	/**
-	 * @param currentUser the currentUser to set
+	 * @param currentUser
+	 *            the currentUser to set
 	 */
 	public static void setCurrentUser(RMOPP_User currentUser) {
 		RMOPP_Utilitties.currentUser = currentUser;
 	}
-	
-	
-	
+
+	/**
+	 * @return the isInputPortSelected
+	 */
+	public static boolean isInputPortSelected() {
+		return isInputPortSelected;
+	}
+
+	/**
+	 * @param isInputPortSelected
+	 *            the isInputPortSelected to set
+	 */
+	public static void setInputPortSelected(boolean isInputPortSelected) {
+		RMOPP_Utilitties.isInputPortSelected = isInputPortSelected;
+	}
+
+	/**
+	 * @return the startXY
+	 */
+	public static Point getStartXY() {
+		return startXY;
+	}
+
+	/**
+	 * @param startXY
+	 *            the startXY to set
+	 */
+	public static void setStartXY(Point startXY) {
+		RMOPP_Utilitties.startXY = startXY;
+	}
+
+	/**
+	 * @return the endXY
+	 */
+	public static Point getEndXY() {
+		return endXY;
+	}
+
+	/**
+	 * @param endXY
+	 *            the endXY to set
+	 */
+	public static void setEndXY(Point endXY) {
+		RMOPP_Utilitties.endXY = endXY;
+	}
+
+	/**
+	 * @return the drawFlag
+	 */
+	public static boolean isDrawFlag() {
+		return drawFlag;
+	}
+
+	/**
+	 * @param drawFlag
+	 *            the drawFlag to set
+	 */
+	public static void setDrawFlag(boolean drawFlag) {
+		RMOPP_Utilitties.drawFlag = drawFlag;
+	}
+
 }
